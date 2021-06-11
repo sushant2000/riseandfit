f1f33d7877435e3be1a3f6d07be8a67c91bfea9e
Mr-man7352 <manissh7352@gmail.com>
2021-06-11 23:41:44 +0530

    added top slider

--- js/headerSlider.js
+++ js/headerSlider.js
@@ -1,19 +1,3 @@
-showSlides();
-var value = "0"
-
-function showSlides() {
-    let slider__inner = document.getElementById("headerslider__inner");
-    if(value == "0"){
-        value = "-100%";
-    }
-
-    else if(value == "-100%"){
-        value = "-200%";
-    }
-
-    else if(value == "-200%"){
-        value = "0";
-    }
-    slider__inner.style.left = value;
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
+$('.carousel').carousel({
+    interval: 4000
+  })
