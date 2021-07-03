0d2bcab696b7db2dc39e3d893553a8f4d99c3d5d
Shubham Singla <singlashubham49@gmail.com>
2021-07-03 19:04:57 +0530

    add new header image

--- Blog/index.php
+++ Blog/index.php
@@ -145,7 +145,7 @@ else{
       <div class="description">
         <h2>Why Rise and fit?</h2>
         <h1 class="border"></h1>
-        <img class="imgRAF" src="./images/Raf/raf.jpg" alt="riseandfit">
+        <img class="imgRAF" src="./images/Raf/raf.png" alt="riseandfit">
         <p class="RAFp">Let's take a step, invest your time and energy, yourself for the
           good of yous and your family.Come join the way to a healthy life. Weaving fitness
           into your busy lifestyle!
